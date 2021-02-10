a32adf4b4e3daa201038339a68428f275be1f682
anujofficial18 <77050622+anujofficial18@users.noreply.github.com>
2021-02-10 19:57:52 +0530

    Add files via upload

---
+++ yearcal.js
@@ -0,0 +1,18 @@
+var sec = 31536000
+
+// var time = {
+//     year:0,
+//     month:0,
+//     days:0,
+//     hour:0,
+//     min:0,
+//     sec:0,
+   
+// }
+var year 
+year =(60*60*24*365)/sec
+console.log(year)
+
+var month
+month =(60*60*24*12)/sec
+console.log(month)
